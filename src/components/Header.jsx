d1ff92367a9c19d6119c803fc89feba6549ab2a0
JosueReyes-1 <josueisrae222@gmail.com>
2023-05-07 22:40:45 -0600

    Se agrego la parte visual de agregar un presupuesto

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { NuevoPresupuesto } from "./NuevoPresupuesto"
+
+export const Header = ({ presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto }) => {
+    return (
+        <header>
+            <h1>Planificador de gastos</h1>
+            {isValidPresupuesto ? (
+                <p>Control Presupuesto</p>
+            ) : (
+                <NuevoPresupuesto
+                    presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto}
+                />
+            )}
+
+        </header>
+    )
+}
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+import { Mensaje } from './Mensaje';
+
+export const NuevoPresupuesto = ({ presupuesto, setPresupuesto,setIsValidPresupuesto }) => {
+    const [mensaje, setMensaje] = useState('');
+
+    const handlerPresupuesto = (e) => {
+        e.preventDefault()
+        if (!presupuesto || presupuesto<0){
+            setMensaje('No es un presupuesto valido')
+            return
+        }
+        setMensaje('')
+        setIsValidPresupuesto(true)
+    }
+
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'>
+            <form onSubmit={handlerPresupuesto} className='formulario'>
+                <div className='campo'>
+                    <label>Definir Presupuesto</label>
+                    <input
+                        type="number"
+                        className='nuevo-presupuesto'
+                        placeholder='Añade tu presupuesto'
+                        value={presupuesto}
+                        onChange={e => setPresupuesto(Number(e.target.value))}
+                    />
+                </div>
+                <input type="submit" value="Añadir" />
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+            </form>
+        </div>
+    )
+}
