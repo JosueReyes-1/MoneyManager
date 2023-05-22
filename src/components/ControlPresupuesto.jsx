92b6e3c5231f119af6c991a549bb5ff7fe2f6066
JosueReyes-1 <josueisrae222@gmail.com>
2023-05-21 23:35:19 -0600

    se implemento boton para reseterar el presupuesto

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,47 +1,63 @@
 import { useEffect, useState } from "react"
-import { CircularProgressbar,buildStyles } from 'react-circular-progressbar'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 
-export const ControlPresupuesto = ({ presupuesto,gastos }) => {
+export const ControlPresupuesto = ({ presupuesto, gastos, setGastos,setPresupuesto,setIsValidPresupuesto }) => {
     const [porcentaje, setPorcentaje] = useState(0);
     const [disponible, setDisponible] = useState(0);
     const [gastado, setGastado] = useState(0);
 
 
     useEffect(() => {
-        const totalGastado=gastos.reduce((total,gasto)=>gasto.cantidad + total,0)
-        const totalDisponible=presupuesto-totalGastado
+        const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
+        const totalDisponible = presupuesto - totalGastado
 
         //calcular el porcentaje gastado
 
-        const nuevoPresuspuesto=(((presupuesto-totalDisponible)/presupuesto)*100).toFixed(2)
+        const nuevoPresuspuesto = (((presupuesto - totalDisponible) / presupuesto) * 100).toFixed(2)
         setDisponible(totalDisponible)
         setGastado(totalGastado)
         setTimeout(() => {
-            setPorcentaje(nuevoPresuspuesto)  
+            setPorcentaje(nuevoPresuspuesto)
         }, 1000);
     }, [gastos]);
 
-    const formatearCantidad=(cantidad)=>{
-        return cantidad.toLocaleString('en-US',{
-            style:'currency',
-            currency:'USD'
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
         })
+    }
+
+    const handlerResetApp=()=>{
+        const resultado =confirm('Deseas reiniciar presupuesto y gastos?')
+        if (resultado){
+            setGastos([])
+            setPresupuesto(0)
+            setIsValidPresupuesto(false)
+        }
     }
     return (
         <div className="contenedor contenedor-presupuesto sombra dos-columnas">
             <div>
-            <CircularProgressbar 
-                styles={buildStyles({
-                    pathColor:porcentaje>100 ? '#DC2626' : '#3B82F6',
-                    trailColor:'#f5f5f5',
-                    textColor:porcentaje>100 ? '#DC2626' : '#3B82F6'
-                })}
-                value={porcentaje}
-                text={`${porcentaje}% Gastado`}
-            />
+                <CircularProgressbar
+                    styles={buildStyles({
+                        pathColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
+                        trailColor: '#f5f5f5',
+                        textColor: porcentaje > 100 ? '#DC2626' : '#3B82F6'
+                    })}
+                    value={porcentaje}
+                    text={`${porcentaje}% Gastado`}
+                />
             </div>
             <div className="contenido-presupuesto">
+                <button
+                    className="reset-app"
+                    type="button"
+                    onClick={handlerResetApp}
+                >
+                    Resetear App
+                </button>
                 <p>
                     <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
                 </p>
